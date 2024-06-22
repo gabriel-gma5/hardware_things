d0aca1322f23f00764acdfd6e91a8ee75deb2847
gabriel-gma5 <gma5@cin.ufpe.br>
2024-06-22 12:13:32 -0300

    intializing repo for cpu project hw

---
+++ parts made/control_unit.v
@@ -0,0 +1,11 @@
+//TODO'S:
+// 1. Listar entradas
+// 2. Listar as saidas por tipo de mux (num. de entradas por mux)
+// 3. " saidas para registradores
+// 4. " saidas para componentes fornecidos
+// 5. " saidas para componentes criados
+// 6.  Definir parametros e variaveis para controlar os estados de cada OpCode
+// 7. Logica de estados para IF e ID
+// 8. Logica de estados por OpCode
+//  8.1 Logica de estados de excecao (OBS.: Overflow tmb incluido em shift's)
+// 9. Revisar 7 e 8 ate ter certeza
---
+++ parts made/div.v
@@ -0,0 +1,3 @@
+// Aqui serao inicializados todos os registrados sem funcao especial:
+//          MDR, A, B, ALUOut, HI, LO, EPC, PC, 
+// Finalidade: sumarizar os componentes .v do projeto
